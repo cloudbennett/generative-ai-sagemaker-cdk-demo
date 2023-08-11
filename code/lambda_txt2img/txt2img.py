10a4ca29ccd2da5ad5edd297124e9af2ba0753ef
Carl Luo <luohuazju@gmail.com>
2023-08-11 10:08:51 -0500

    clean up and update the lambda

--- code/lambda_txt2img/txt2img.py
+++ code/lambda_txt2img/txt2img.py
@@ -1,22 +1,28 @@
 import json
 import boto3
-runtime= boto3.client('runtime.sagemaker') 
+
+runtime = boto3.client('runtime.sagemaker')
 
 
 def lambda_handler(event, context):
     body = json.loads(event['body'])
     prompt = body['prompt']
     endpoint_name = body['endpoint_name']
-    
-    response = runtime.invoke_endpoint(EndpointName=endpoint_name, 
-                                      Body=prompt, 
-                                      ContentType='application/x-text')    
-    
-    response_body = json.loads(response['Body'].read().decode())
-    generated_image = response_body['generated_image']
-    
-    message = {"prompt": prompt,'image':generated_image}
-    
+
+    prompt_payload = {
+        "text_prompts": [
+        {
+            "text": prompt,
+            "weight": 1
+        }
+    ]}
+    response = runtime.invoke_endpoint(EndpointName=endpoint_name,
+                                            Body=json.dumps(prompt_payload),
+                                            ContentType='application/json')
+    response_body = json.loads(response['Body'].read())
+    generated_image = response_body["artifacts"][0]["base64"]
+
+    message = {"prompt": prompt, 'image': generated_image}
     return {
         "statusCode": 200,
         "body": json.dumps(message),
